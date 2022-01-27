a8dc2feba1aa82254d59ca8449f02d2dcb9db6e0
Fasil <Fasil.alemeye@gmail.com>
2022-01-27 13:19:34 +0300

    Mvc initial

---
+++ CleanArchitecture/UltraMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UltraMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
